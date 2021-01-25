4d81f411e79590499399e74e8079087573d779bc
Benny Gonzalez <ohnobenito@gmail.com>
2021-01-25 10:02:29 -0600

    Delete seeds.sql

--- seeds.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO chats (topic) VALUES ('General');
-INSERT INTO chats (topic) VALUES ('Music');
-INSERT INTO chats (topic) VALUES ('Movies');
-INSERT INTO chats (topic) VALUES ('Sports');
